66c4a83a63fad83317c09a1b6b9d19e2147f2242
Shwetha-15 <shwetha151998@gmail.com>
2022-03-03 09:14:05 +0530

    1st commit

--- JDBC-PROJECT/src/jdbcProject_1st_March/JDBCDemo2.java
+++ JDBC-PROJECT/src/jdbcProject_1st_March/JDBCDemo2.java
@@ -1,49 +1,40 @@
 package jdbcProject_1st_March;
 import java.sql.*;
-public class JDBCDemo1 
+public class JDBCDemo2 
 {
 	public static void main(String[] args) throws Exception 
 	{
 		// TODO Auto-generated method stub
         Class.forName("com.mysql.cj.jdbc.Driver");
         
-        Connection con=DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/student", "root", "root");
+        Connection con=DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/student-1", "root", "root");
         
         Statement st=con.createStatement();
         
         //String query="create table student(id int primary key,sname varchar(20))";
-        try 
-        {
+    try 
+    {
         String query1="insert into student values(1,'Sumanth')";
-        //String query2="insert into student values(2,'Shwetha')";
-        //String query3="select * from student";
-        //String query4="delete from student where id=2";
         System.out.println("id\tname\t");
         ResultSet rs=st.executeQuery("Select id,sname from student");
         while(rs.next())
         {
-//        	System.out.println(rs.getString(1));
-//        	System.out.println(rs.getString(2));
-//        	System.out.println(rs.getString(3));
         	int id=rs.getInt("id");
         	String sname=rs.getString("sname");
         	System.out.println(id + "\t" + sname);
         }
-        } 
-        catch (Exception e) 
-        {
-			// TODO: handle exception
-        	System.out.println(e);
-		}
+    } 
+    catch (Exception e) 
+    {
+		// TODO: handle exception
+    	System.out.println(e);
+	}
         
         //st.executeUpdate(query);
         //st.executeUpdate(query1);
-        //st.executeUpdate(query2);
-        //st.executeUpdate(query3);
-        //st.executeUpdate(query4);
         
-        //System.out.println("student table created successfully!!!!");
-        System.out.println("Row inserted successfully!!!!");
+        //System.out.println("student-1 table created successfully!!!!");
+        //System.out.println("Row inserted successfully!!!!");
         
         st.close();
         con.close();
