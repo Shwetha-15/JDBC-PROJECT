9596f317270ac4a5158aadc2914805342395c4b9
Shwetha-15 <shwetha151998@gmail.com>
2022-03-08 19:48:50 +0530

    1st commit

--- JDBC-PROJECT/src/march_3/JunitDemo1.java
+++ JDBC-PROJECT/src/march_3/JunitDemo1.java
@@ -1,5 +1,7 @@
 package march_3;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNotSame;
 import static org.junit.jupiter.api.Assertions.assertNull;
@@ -15,12 +17,16 @@ public class JunitDemo1
 	    String s2="Junit";
 	    String s3="Test";
 	    String s4=null;
+	    
 	    int var1=1;
 	    int var2=2;
-	
-	    assertEquals(s1,s2);  //Runs
-	    //assertEquals(s1,s3); //Fails
-	    assertSame( s1,s3);
+	    
+	    
+	    assertEquals(s1,s2);  
+	    assertTrue(var1 < var2);
+	    assertFalse(var1 > var2);
+	    assertEquals(s1,s2); 
+	    assertSame( s1,s2);
 	    assertNotSame(s2, s4);
 	    assertNotNull(s1);
 	    assertNull(s4);
