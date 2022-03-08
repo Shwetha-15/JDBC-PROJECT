6e2f78d5ab64ef208236f8f0d1117ef0a08a0fa0
Shwetha-15 <shwetha151998@gmail.com>
2022-03-08 20:03:38 +0530

    8th commit

---
+++ JDBC-PROJECT/src/march_7/RepeatingTest.java
@@ -0,0 +1,9 @@
+package march_7;
+import org.junit.jupiter.api.RepeatedTest;
+public class RepeatingTest 
+{
+	@RepeatedTest(50)
+	void test() {
+		System.out.println("First Test Case .....");
+	}
+}
