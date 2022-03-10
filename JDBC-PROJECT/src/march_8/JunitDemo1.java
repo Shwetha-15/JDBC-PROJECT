4e636b80e8f9f6644661ba8821e7fb24a7c144bb
Shwetha-15 <shwetha151998@gmail.com>
2022-03-11 00:45:54 +0530

    1st commit

---
+++ JDBC-PROJECT/src/march_8/JunitDemo1.java
@@ -0,0 +1,11 @@
+package march_8;
+import static org.junit.Assert.assertTrue;
+import org.junit.jupiter.api.Test;
+public class JunitDemo1 
+{
+	@Test
+    void demoTestMethod() 
+	{
+        assertTrue(true);
+    }
+}
---
+++ JDBC-PROJECT/src/march_8/JunitDemo2.java
@@ -0,0 +1,21 @@
+package march_8;
+import static org.junit.Assert.assertTrue;
+import org.junit.jupiter.api.Test;
+public class JunitDemo2 
+{
+	@Test
+	public void testAddInt()
+	{
+		System.out.println("not yet completed");
+	}
+	@Test
+	public void testIndexOf()
+	{
+		System.out.println("not yet completed");
+	}
+	@Test
+	public void testSize()
+	{
+		System.out.println("not yet completed");
+	}
+}
